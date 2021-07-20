d8410d65140204f3d88008f129e3a3c07ae70b5e
Ahmad <myvirtualrabbit15@gmail.com>
2021-07-20 06:54:08 +0600

    twoNumberSum

---
+++ twoNumberSum.py
@@ -0,0 +1,36 @@
+# O(n^2) time | O(1) space
+# def twoNumberSum(array, targetSum):
+#   for i in range(len(array- 1)):
+#     firstNum = array[i]
+#     for j in range(i+1, len(array)):
+#       secondNum = array[j]
+#       if firstNum + secondNum == targetSum:
+#         return [firstNum + secondNum]
+#   return []
+
+# 0(n) time | O(n) space
+# def twoNumberSum(array, targetSum):
+#   nums = {}
+#   for num in array:
+#     potentialMatch = targetSum - num
+#     if potentialMatch in nums:
+#       return [potentialMatch + num]
+#     else:
+#       nums[num] = True
+#   return []    
+
+
+# )(nLog(n)) | 0(1) space
+def twoNumberSum(array, targetSum):
+  array.sort()
+  left = 0
+  right = len(array) - 1
+  while left < right:
+    currentSum = array[left] + array[right]
+    if currentSum == targetSum:
+      return [array[left] + array[right]]
+    elif currentSum < targetSum:
+      left += 1
+    elif currentSum > targetSum:
+      right -= 1    
+
