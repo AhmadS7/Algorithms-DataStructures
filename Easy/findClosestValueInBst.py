2abd6f3a53861f4e93e3fd3ff08d7717abf320db
Ahmad <myvirtualrabbit15@gmail.com>
2021-08-24 08:42:03 +0600

    second Solution

--- Easy/findClosestValueInBst.py
+++ Easy/findClosestValueInBst.py
@@ -18,22 +18,40 @@ if __name__ == '__main__':
 	root.right.left.right = newnode(14) 
 	k = 12
 
+# Avarage: O(log(n)) | O(log(n))
+# Worst: O(n) time | O(n) space
+# def findClosestValueInBst(tree, target):
+#     return findClosestValueInBstHelper(tree,target, float("inf"))
 
+# def findClosestValueInBstHelper(tree, target, closest):
+#     if tree is None:
+#         return closest
+#     if abs(target - closest) > abs(target - tree.value):
+#         return tree.value
+#     if target < tree.value:
+#         return findClosestValueInBstHelper(tree.left, target, closest)
+#     elif target > tree.value:
+#         return findClosestValueInBstHelper(tree.right, target, closest) 
+#     else:
+#         return closest
+
+# Avarage: O(log(n)) | O(log(n))
+# Worst: O(n) time | O(1) space
 def findClosestValueInBst(tree, target):
     return findClosestValueInBstHelper(tree,target, float("inf"))
 
 def findClosestValueInBstHelper(tree, target, closest):
-    if tree is None:
-        return closest
-    if abs(target - closest) > abs(target - tree.value):
-        closest = tree.value
-    if target < tree.value:
-        return findClosestValueInBstHelper(tree.left, target, closest)
-    elif target > tree.value:
-        return findClosestValueInBstHelper(tree.right, target, closest)
-    else:
-        return closest
-
+    currentNode = tree
+    while currentNode is not None:
+        if abs(target - closest) > abs(target - currentNode.value):
+            closest = currentNode.value
+        if target < currentNode.value:
+           currentNode = currentNode.left
+        elif target > currentNode.value:
+           currentNode = currentNode.right 
+        else:
+            break
+    return closest
 
 
 print(findClosestValueInBst(root, k))
